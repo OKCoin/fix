61c49fbc7d3c9d256fe00b0a42e0a164494e8b5d
OKCoin <liang.yu@okcoin.com>
2015-09-09 15:39:37 +0800

    Fix 支持C#

---
+++ c#/FixDemo/AccountUtil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuickFix;
+namespace FixDemo
+{
+    public class AccountUtil
+    {
+       public static  String apiKey ="8bd8ae46-756e-11e4-acdf-00163e0238cc";
+	   public static  String sercretKey="22184567C5152DC2FFE02155C69CFC85";
+    }
+}
